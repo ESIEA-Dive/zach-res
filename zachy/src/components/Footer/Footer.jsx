f77d3d998b5e15d36df7f476d14ddcfff58b1cc3
Alexandre Dive <adive.2015@gmail.com>
2024-03-28 17:02:08 -0400

    thingy

--- zachy/src/components/Footer/Footer.jsx
+++ zachy/src/components/Footer/Footer.jsx
@@ -13,10 +13,10 @@ const footer = ({ setIsHovered }) => {
 
   return (
     <div className='mg-top-b app-center content' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <a className='links' href="mailto:zach_dive@berkeley.edu" target="_blank">zach_dive@berkeley.edu</a>
-      <a className='links' href="https://x.com/zachdive?s=11&t=BorR7lwfN2Yl_k0dZdewww" target="_blank">twitter</a>
-      <a className='links' href="https://instagram.com" target="_blank">instagram</a>
-      <a className='links' href="https://github.com" target="_blank">github</a>
+      <a className='links-f' href="mailto:zach_dive@berkeley.edu" target="_blank">zach_dive@berkeley.edu</a>
+      <a className='links-f' href="https://x.com/zachdive?s=11&t=BorR7lwfN2Yl_k0dZdewww" target="_blank">twitter</a>
+      <a className='links-f' href="https://www.linkedin.com/in/zacharydive?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">linkedin</a>
+      <a className='links-f' href="https://github.com/zachdive" target="_blank">github</a>
     </div>
   );
 }
--- zachy/src/components/Projects/Projects.jsx
+++ zachy/src/components/Projects/Projects.jsx
@@ -11,27 +11,27 @@ const projects = ({ setIsHovered }) => {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
-  
+
   return (
   <div className='mg-top-b'>
     <div>
       <h2>personal projects</h2>
       
-      <ol onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='mg-top-m'>
-        <li>adam: generative computer-aided design</li>
-        <li>dreamscapes: a vr gaussian-splatting editor</li>
-        <li>mint: order from local bay area farmers' markets</li>
-        <li>talesai: generative stories for children</li>
+      <ol className='mg-top-m'>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">adam: generative computer-aided design</a></li>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">dreamscapes: a vr gaussian-splatting editor</a></li>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">mint: order from local bay area farmers' markets</a></li>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">talesai: generative stories for children</a></li>
       </ol>
     </div>
 
     <div className='mg-top-m'>
       <h2>past projects</h2>
 
-      <ol onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='mg-top-m'>
-        <li>junctionx exeter: a large-scale hackathon</li>
-        <li>excode: free coding bootcamp at the university of exeter</li>
-        <li>exeter entrepreneurs: a community of student entrepreneurs</li>
+      <ol className='mg-top-m'>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">junctionx exeter: a large-scale hackathon</a></li>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">excode: free coding bootcamp at the university of exeter</a></li>
+        <li><a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href="#" class="links">exeter entrepreneurs: a community of student entrepreneurs</a></li>
       </ol>
     </div>
   </div>
