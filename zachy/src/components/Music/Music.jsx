70d3a061a089db567f96f30567227d5677162ec0
Alexandre Dive <adive.2015@gmail.com>
2024-03-29 16:24:23 -0400

    lol

--- zachy/src/components/Music/Music.jsx
+++ zachy/src/components/Music/Music.jsx
@@ -6,6 +6,8 @@ const music = () => {
   return (
     <div className='mg-top-m'>
       <p>#now-playing -</p>
+      <br/>
+      <p className='lol'>my name is zach and i am fully gay mate ! Oi Bruv gimme a pint u wanker, bloody hell !</p>
     </div>
   );
 }
